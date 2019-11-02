b08da4e45c0d3d51ffa9df7a01947e1bf2599a11
David-PC <dk9946@student.uni-lj.si>
2019-11-02 07:57:45 +0100

    cleaned imports

--- app/src/main/java/com/example/fueldiet/Activity/ChartsActivity.java
+++ app/src/main/java/com/example/fueldiet/Activity/ChartsActivity.java
@@ -1,58 +1,16 @@
 package com.example.fueldiet.Activity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.NumberPicker;
-import android.widget.Spinner;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.fueldiet.Fragment.BarChartFragment;
 import com.example.fueldiet.Fragment.LineChartFragment;
-import com.example.fueldiet.Fragment.MonthYearPickerFragment;
 import com.example.fueldiet.Fragment.PieChartFragment;
 import com.example.fueldiet.R;
-import com.example.fueldiet.Utils;
-import com.example.fueldiet.db.FuelDietContract;
-import com.example.fueldiet.db.FuelDietDBHelper;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class ChartsActivity extends AppCompatActivity {
 
