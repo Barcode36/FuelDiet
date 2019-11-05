3cd5099dc87ecf3de6ed986582fbfa0366626cd8
David-PC <dk9946@student.uni-lj.si>
2019-11-05 13:39:39 +0100

    are you sure dialog for reminder delete

--- app/src/main/java/com/example/fueldiet/Fragment/VehicleReminderFragment.java
+++ app/src/main/java/com/example/fueldiet/Fragment/VehicleReminderFragment.java
@@ -1,6 +1,7 @@
 package com.example.fueldiet.Fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -10,11 +11,13 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fueldiet.Activity.AddNewReminderActivity;
+import com.example.fueldiet.Activity.MainActivity;
 import com.example.fueldiet.Activity.VehicleDetailsActivity;
 import com.example.fueldiet.Adapter.ReminderMultipleTypeAdapter;
 import com.example.fueldiet.Object.ReminderObject;
@@ -36,6 +39,9 @@ public class VehicleReminderFragment extends Fragment {
     View view;
     FloatingActionButton fab;
     List<ReminderObject> reminderList;
+
+    private int tmpPos;
+    private int tmpItm;
 
     public VehicleReminderFragment() {}
 
@@ -80,10 +86,11 @@ public class VehicleReminderFragment extends Fragment {
 
             @Override
             public void onDeleteClick(int position, int element_id) {
-                delete(element_id);
-                Toast.makeText(getContext(), "Position: "+position, Toast.LENGTH_SHORT).show();
-                fillRemindersList();
-                mAdapter.notifyItemRemoved(position);
+                tmpItm = element_id;
+                tmpPos = position;
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage(getString(R.string.are_you_sure)).setPositiveButton(getString(R.string.yes), dialogClickListener)
+                        .setNegativeButton(getString(R.string.no), dialogClickListener).show();;
             }
 
             @Override
@@ -118,6 +125,25 @@ public class VehicleReminderFragment extends Fragment {
         });
 
         return view;
+    }
+
+    DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
+        switch (which){
+            case DialogInterface.BUTTON_POSITIVE:
+                removeItem();
+                break;
+
+            case DialogInterface.BUTTON_NEGATIVE:
+                Toast.makeText(getContext(), getString(R.string.canceled), Toast.LENGTH_SHORT).show();
+                break;
+        }
+    };
+
+    private void removeItem() {
+        delete(tmpItm);
+        Toast.makeText(getContext(), "Object deleted!", Toast.LENGTH_SHORT).show();
+        fillRemindersList();
+        mAdapter.notifyItemRemoved(tmpPos);
     }
 
     private boolean fillRemindersList() {
