de22ac28324e8f6bfe8db5d1880fac7483d589d8
David <dk9946@student.uni-lj.si>
2020-02-06 20:02:45 +0100

    added calculator and general ui fixes

--- app/src/main/java/com/example/fueldiet/Fragment/CalculatorFragment.java
+++ app/src/main/java/com/example/fueldiet/Fragment/CalculatorFragment.java
@@ -10,8 +10,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.fueldiet.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CalculatorFragment extends Fragment {
+
+    TextInputLayout km;
+    TextInputLayout litre;
+    TextInputLayout price;
+    TextInputLayout litrePrice;
+    TextInputLayout consumption;
 
     public static CalculatorFragment newInstance() {
         CalculatorFragment fragment = new CalculatorFragment();
@@ -28,7 +35,11 @@ public class CalculatorFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calculator, container, false);
 
-
+        km = view.findViewById(R.id.calc_km_input);
+        litre = view.findViewById(R.id.calc_litres_input);
+        price = view.findViewById(R.id.calc_total_cost_input);
+        litrePrice = view.findViewById(R.id.calc_price_per_l_input);
+        consumption = view.findViewById(R.id.calc_cons_input);
 
         return view;
     }
