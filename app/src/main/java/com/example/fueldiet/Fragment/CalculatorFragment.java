0f4dd232cdd05d78623f53356205d8f0dc7744f0
David <dk9946@student.uni-lj.si>
2020-02-07 09:03:25 +0100

    1. half of calculator is working

--- app/src/main/java/com/example/fueldiet/Fragment/CalculatorFragment.java
+++ app/src/main/java/com/example/fueldiet/Fragment/CalculatorFragment.java
@@ -1,16 +1,24 @@
 package com.example.fueldiet.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.fueldiet.R;
+import com.example.fueldiet.Utils;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculatorFragment extends Fragment {
 
@@ -19,6 +27,14 @@ public class CalculatorFragment extends Fragment {
     TextInputLayout price;
     TextInputLayout litrePrice;
     TextInputLayout consumption;
+
+    String recent = "";
+
+    TextWatcher forKm;
+    TextWatcher forLitre;
+    TextWatcher forPrice;
+    TextWatcher forLitrePrice;
+    TextWatcher forCons;
 
     public static CalculatorFragment newInstance() {
         CalculatorFragment fragment = new CalculatorFragment();
@@ -41,12 +57,140 @@ public class CalculatorFragment extends Fragment {
         litrePrice = view.findViewById(R.id.calc_price_per_l_input);
         consumption = view.findViewById(R.id.calc_cons_input);
 
-        createListeners();
+        createTextWatchers();
 
         return view;
     }
 
-    private void createListeners() {
+    private void createTextWatchers() {
+        forKm = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!litre.getEditText().getText().toString().equals("") && !recent.equals("litre") && s.length() > 0) {
+                    Double cons = Utils.calculateConsumption(Integer.parseInt(km.getEditText().getText().toString()), Double.parseDouble(litre.getEditText().getText().toString()));
+                    removeTextWatcherConsumption();
+                    consumption.getEditText().setText(cons+"");
+                    recent = "consumption";
+                    addTextWatcherConsumption();
+                } else if (!consumption.getEditText().getText().toString().equals("") && !recent.equals("consumption") && s.length() > 0) {
+                    BigDecimal cons = new BigDecimal(consumption.getEditText().getText().toString());
+                    cons = cons.divide(BigDecimal.valueOf(100));
+                    cons = cons.multiply(new BigDecimal(s.toString()));
+                    cons = cons.setScale(2, RoundingMode.HALF_UP);
+                    removeTextWatcherLitre();
+                    litre.getEditText().setText(cons.toString());
+                    recent = "litre";
+                    addTextWatcherLitre();
+                }
+            }
+        };
+        forCons = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!km.getEditText().getText().toString().equals("") && !recent.equals("km") && s.length() > 0) {
+                    BigDecimal cons = new BigDecimal(consumption.getEditText().getText().toString());
+                    cons = cons.divide(BigDecimal.valueOf(100));
+                    cons = cons.multiply(new BigDecimal(km.getEditText().getText().toString()));
+                    cons = cons.setScale(2, RoundingMode.HALF_UP);
+                    removeTextWatcherLitre();
+                    litre.getEditText().setText(cons.toString());
+                    recent = "litre";
+                    addTextWatcherLitre();
+                } else if (!litre.getEditText().getText().toString().equals("") && !recent.equals("litre") && s.length() > 0) {
+                    BigDecimal dist = new BigDecimal(litre.getEditText().getText().toString());
+                    String cons = consumption.getEditText().getText().toString();
+                    dist = dist.divide(new BigDecimal(cons), RoundingMode.HALF_UP);
+                    dist = dist.multiply(BigDecimal.valueOf(100));
+                    dist = dist.setScale(2, RoundingMode.HALF_UP);
+
+                    removeTextWatcherKm();
+                    km.getEditText().setText(dist.toString());
+                    recent = "km";
+                    addTextWatcherKm();
+                }
+            }
+        };
+
+        forLitre = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!km.getEditText().getText().toString().equals("") && !recent.equals("km") && s.length() > 0) {
+                    Double cons = Utils.calculateConsumption(Integer.parseInt(km.getEditText().getText().toString()), Double.parseDouble(litre.getEditText().getText().toString()));
+                    removeTextWatcherConsumption();
+                    consumption.getEditText().setText(cons+"");
+                    recent = "consumption";
+                    addTextWatcherConsumption();
+                } else if (!consumption.getEditText().getText().toString().equals("") && !recent.equals("consumption") && s.length() > 0) {
+                    BigDecimal dist = new BigDecimal(litre.getEditText().getText().toString());
+                    String tmp = litre.getEditText().getText().toString();
+                    BigDecimal tmp1 = new BigDecimal(litre.getEditText().getText().toString());
+                    dist = dist.divide(new BigDecimal(consumption.getEditText().getText().toString()), RoundingMode.HALF_UP);
+                    dist = dist.multiply(BigDecimal.valueOf(100));
+                    dist = dist.setScale(2, RoundingMode.HALF_UP);
+                    removeTextWatcherKm();
+                    km.getEditText().setText(dist.toString());
+                    recent = "km";
+                    addTextWatcherKm();
+                }
+            }
+        };
+
+        addTextWatcherKm();
+        addTextWatcherConsumption();
+        addTextWatcherLitre();
+    }
+
+    private void removeTextWatcherConsumption() {
+        consumption.getEditText().removeTextChangedListener(forCons);
+    }
+
+    private void addTextWatcherConsumption() {
+        consumption.getEditText().addTextChangedListener(forCons);
+    }
+    
+    private void removeTextWatcherKm() {
+        km.getEditText().removeTextChangedListener(forKm);
+    }
+
+    private void addTextWatcherKm() {
+        km.getEditText().addTextChangedListener(forKm);
+    }
+
+    private void removeTextWatcherLitre() {
+        litre.getEditText().removeTextChangedListener(forLitre);
+    }
+
+    private void addTextWatcherLitre() {
+        litre.getEditText().addTextChangedListener(forLitre);
     }
 }
