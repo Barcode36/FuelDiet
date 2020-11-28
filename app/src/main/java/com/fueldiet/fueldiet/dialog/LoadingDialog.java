d931915f349c0c8587d423b05d3c5172b703a2fe
David <dk9946@student.uni-lj.si>
2020-11-28 08:00:08 +0100

    new loading dialog

---
+++ app/src/main/java/com/fueldiet/fueldiet/dialog/LoadingDialog.java
@@ -0,0 +1,35 @@
+package com.fueldiet.fueldiet.dialog;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.fueldiet.fueldiet.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class LoadingDialog extends AppCompatDialogFragment {
+
+    private Activity activity;
+    private AlertDialog dialog;
+
+    public LoadingDialog(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void showDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(activity);
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog_loading, null));
+        builder.setCancelable(false);
+
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    public void hideDialog() {
+        dialog.dismiss();
+    }
+}
