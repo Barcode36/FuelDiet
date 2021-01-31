3c1174601f62cf3dc98a28f603a647d1afcd3111
David <dk9946@student.uni-lj.si>
2021-01-31 09:37:14 +0100

    option to display success and failure

--- app/src/main/java/com/fueldiet/fueldiet/dialog/LoadingDialog.java
+++ app/src/main/java/com/fueldiet/fueldiet/dialog/LoadingDialog.java
@@ -2,6 +2,8 @@ package com.fueldiet.fueldiet.dialog;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
@@ -35,5 +37,21 @@ public class LoadingDialog extends AppCompatDialogFragment {
 
     public boolean isDisplayed() {
         return dialog != null && dialog.isShowing();
+    }
+
+    public void setSuccessful() {
+        dialog.findViewById(R.id.dialog_loading_indicator).setVisibility(View.INVISIBLE);
+        dialog.findViewById(R.id.dialog_loading_success_img).setVisibility(View.VISIBLE);
+        TextView textView = dialog.findViewById(R.id.dialog_loading_text);
+        textView.setText(activity.getString(R.string.success));
+        dialog.findViewById(R.id.dialog_loading_unit).setVisibility(View.INVISIBLE);
+    }
+
+    public void setError() {
+        dialog.findViewById(R.id.dialog_loading_indicator).setVisibility(View.INVISIBLE);
+        dialog.findViewById(R.id.dialog_loading_failed_img).setVisibility(View.VISIBLE);
+        TextView textView = dialog.findViewById(R.id.dialog_loading_text);
+        textView.setText(activity.getString(R.string.error));
+        dialog.findViewById(R.id.dialog_loading_unit).setVisibility(View.INVISIBLE);
     }
 }
