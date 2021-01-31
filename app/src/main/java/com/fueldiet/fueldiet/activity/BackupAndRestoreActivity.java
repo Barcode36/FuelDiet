ae09e1e60f083935a4447c89dc63103427304738
David <dk9946@student.uni-lj.si>
2021-01-31 09:38:16 +0100

    code refactoring

--- app/src/main/java/com/fueldiet/fueldiet/activity/BackupAndRestoreActivity.java
+++ app/src/main/java/com/fueldiet/fueldiet/activity/BackupAndRestoreActivity.java
@@ -7,6 +7,8 @@ import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +25,8 @@ import com.fueldiet.fueldiet.AutomaticBackup;
 import com.fueldiet.fueldiet.R;
 import com.fueldiet.fueldiet.Utils;
 import com.fueldiet.fueldiet.adapter.FoldersAdapter;
+import com.fueldiet.fueldiet.dialog.LoadingDialog;
+import com.fueldiet.fueldiet.utils.AsyncTaskCoroutine;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
@@ -39,21 +43,29 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class BackupAndRestore extends BaseActivity {
+public class BackupAndRestoreActivity extends BaseActivity {
     private static final String TAG = "BackupAndRestore";
     private static final int RESTORE_CODE = 0;
     private static final int BACKUP_CODE = 1;
     private static final int SAVE_AS_CODE = 2;
+    private static final String TYPE = "text/*";
+    private static final String DO_IN_BACKGROUND = "doInBackground";
+    private static final String POST_STARTED = "onPostExecute: started...";
+    private static final String POST_FINISHED = "onPostExecute: finished";
+    private static final String PRE_STARTED = "onPreExecute: started...";
+    private static final String PRE_FINISHED = "onPreExecute: finished";
 
-    MaterialButton backup, restore;
+    MaterialButton backup;
+    MaterialButton restore;
     TextView defaultDir;
-    RecyclerView mRecyclerView;
-    LinearLayoutManager mLayoutManager;
-    FoldersAdapter mAdapter;
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    FoldersAdapter adapter;
     AutomaticBackup automaticBackup;
     List<File> data;
     Locale locale;
     File backupToMove;
+    LoadingDialog loadingDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,23 +74,38 @@ public class BackupAndRestore extends BaseActivity {
         backupToMove = null;
 
         setContentView(R.layout.activity_backup_and_restore);
-        automaticBackup = new AutomaticBackup(this);
+        automaticBackup = new AutomaticBackup(this, locale);
 
         Configuration configuration = getResources().getConfiguration();
         locale = configuration.getLocales().get(0);
 
-        Log.d(TAG, "onCreate: linking fields");
+        setTitle(R.string.backup_and_restore);
+
+        initVariables();
+        getData();
+        initAdapter();
+        addClickListeners();
+        finishSetUp();
+        
+        Log.d(TAG, "onCreate: finished");
+    }
+    
+    private void initVariables() {
+        Log.d(TAG, "initVariables: started...");
         backup = findViewById(R.id.activity_backup_button_backup);
         restore = findViewById(R.id.activity_backup_button_restore);
         defaultDir = findViewById(R.id.activity_backup_textview_folder);
-        mRecyclerView = findViewById(R.id.activity_backup_recyclerview_restore);
+        recyclerView = findViewById(R.id.activity_backup_recyclerview_restore);
         data = new ArrayList<>();
-
-        fillData();
-
-        mLayoutManager= new LinearLayoutManager(this);
-        mAdapter = new FoldersAdapter(this, data);
-        mAdapter.setOnItemClickListener(new FoldersAdapter.OnItemClickListener() {
+        loadingDialog = new LoadingDialog(this);
+        Log.d(TAG, "initVariables: finished");
+    }
+    
+    private void initAdapter() {
+        Log.d(TAG, "initAdapter: started...");
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new FoldersAdapter(this, data);
+        adapter.setOnItemClickListener(new FoldersAdapter.OnItemClickListener() {
             @Override
             public void onItemClickOld(int position) {
                 selectFolderOld(position);
@@ -89,10 +116,11 @@ public class BackupAndRestore extends BaseActivity {
                 selectFolderNew(position);
             }
         });
-
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
+        Log.d(TAG, "initAdapter: finished");
+    }
+    
+    private void addClickListeners() {
+        Log.d(TAG, "addClickListeners: started...");
         Context context = this;
 
         backup.setOnClickListener(v -> {
@@ -104,12 +132,10 @@ public class BackupAndRestore extends BaseActivity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("text/*");
+                intent.setType(TYPE);
                 intent.putExtra(Intent.EXTRA_TITLE, String.format(locale,"fueldiet_%s.csv", formattedDate));
-
                 startActivityForResult(intent, BACKUP_CODE);
             } else {
-
                 MaterialAlertDialogBuilder alert = new MaterialAlertDialogBuilder(context);
                 final EditText edittext = new EditText(context);
                 alert.setTitle(R.string.enter_backup_file_name);
@@ -122,33 +148,30 @@ public class BackupAndRestore extends BaseActivity {
                     String fileName = edittext.getText().toString();
                     File newBackup = new File(automaticBackup.backupDir.getAbsolutePath() + "/" + fileName);
                     dialog.dismiss();
-                    Intent passData = new Intent();
-                    //---set the data to pass back---
-                    passData.setData(Uri.fromFile(newBackup));
-                    setResult(MainActivity.RESULT_BACKUP, passData);
-                    //---close the activity---
-                    finish();
+                    backup(Uri.fromFile(newBackup));
                 });
-
                 alert.setNegativeButton(context.getString(R.string.cancel), (dialog, whichButton) -> dialog.cancel());
-
                 alert.show();
             }
         });
-
         restore.setOnClickListener(v -> {
             Intent fileSource = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             fileSource.addCategory(Intent.CATEGORY_OPENABLE);
-            fileSource.setType("text/*");
+            fileSource.setType(TYPE);
 
-            //Uri uri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", automaticBackup.backupDir);
-            //fileDest.setDataAndType(uri, "text/*");
             try {
                 startActivityForResult(fileSource, RESTORE_CODE);
             } catch (ActivityNotFoundException e) {
                 Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+        Log.d(TAG, "addClickListeners: finished");
+    }
+    
+    private void finishSetUp() {
+        Log.d(TAG, "finishSetUp: started...");
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
 
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
@@ -158,7 +181,7 @@ public class BackupAndRestore extends BaseActivity {
         } catch (IOException e) {
             defaultDir.setText(getString(R.string.error));
         }
-        Log.d(TAG, "onCreate: finished");
+        Log.d(TAG, "finishSetUp: finished");
     }
 
     @Override
@@ -166,80 +189,51 @@ public class BackupAndRestore extends BaseActivity {
         super.onActivityResult(requestCode, resultCode, data);
         Log.d(TAG, String.format("onActivityResult: started with request: %d", requestCode));
 
-        if (requestCode == RESTORE_CODE) {
-            if (resultCode == RESULT_OK) {
-                if (data != null && data.getData() != null) {
-                    Log.d(TAG, "onActivityResult: started to restore");
-                    Uri uri = data.getData();
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                        Log.d(TAG, "onActivityResult: restoring on android 10 or higher");
-                        try {
-                            InputStream inputStream = getContentResolver().openInputStream(uri);
-                            assert inputStream != null;
-                            String msg = Utils.readCSVfile(inputStream, getApplicationContext());
-                            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-                            setResult(MainActivity.RESULT_RESTORE_10_UP);
-                        } catch (FileNotFoundException e) {
-                            Log.e(TAG, "onActivityResult: file was not found on restore backup", e.fillInStackTrace());
-                            e.printStackTrace();
-                        }
-                    } else {
-                        Log.d(TAG, "onActivityResult: restoring on android 9 or lower");
-                        Intent passData = new Intent();
-                        //---set the data to pass back---
-                        passData.setData(uri);
-                        setResult(MainActivity.RESULT_RESTORE, passData);
-                        //---close the activity---
-                        finish();
-                    }
-                }
-            }
-        } else if (requestCode == BACKUP_CODE) {
+        if (requestCode == RESTORE_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Log.d(TAG, "onActivityResult: restore");
+            Uri uri = data.getData();
+            restore(uri);
+        } else if (requestCode == BACKUP_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            //only android 10 and up can end up here
+            Log.d(TAG, "onActivityResult: backup");
+            Uri uri = data.getData();
+            backup(uri);
+        } else if (requestCode == SAVE_AS_CODE && resultCode == RESULT_OK && data != null && data.getData() != null && backupToMove != null) {
             //only for android 10 and up
-            if (resultCode == RESULT_OK) {
-                if (data != null && data.getData() != null) {
-                    Log.d(TAG, "onActivityResult: started to backup on android 10 or higher");
-                    Uri uri = data.getData();
-                    try {
-                        OutputStream outputStream = getContentResolver().openOutputStream(uri);
-                        assert outputStream != null;
-                        String msg = Utils.createCSVfile(outputStream, getApplicationContext());
-                        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-                    } catch (FileNotFoundException e) {
-                        Log.e(TAG, "onActivityResult: file was not found on create backup", e.fillInStackTrace());
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } else if (requestCode == SAVE_AS_CODE) {
-            //only for android 10 and up
-            if (resultCode == RESULT_OK) {
-                if (data != null && data.getData() != null && backupToMove != null) {
-                    Log.d(TAG, "onActivityResult: started to copy backup file");
-                    //save file from app storage to user selected folder
-                    try {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            OutputStream outputStream = getContentResolver().openOutputStream(data.getData());
-                            assert outputStream != null;
-                            outputStream.write(Files.readAllBytes(backupToMove.toPath()));
-                            outputStream.close();
-                            Toast.makeText(this, getString(R.string.backup_created), Toast.LENGTH_LONG).show();
-                        }
-                    } catch (IOException e) {
-                        Log.e(TAG, "onActivityResult: error when moving file from app storage to user defined location", e.fillInStackTrace());
-                        e.printStackTrace();
-                    }
-                }
-            }
+            Log.d(TAG, "onActivityResult: save as file");
+            //save file from app storage to user selected folder
+            Uri uri = data.getData();
+            saveFile(uri);
         }
     }
 
-    private void fillData() {
-        Log.d(TAG, "fillData: started");
+    private void restore(Uri uri) {
+        Log.d(TAG, "restore: started...");
+        RestoreCoroutine restoreCoroutine = new RestoreCoroutine();
+        restoreCoroutine.execute(uri);
+        Log.d(TAG, "restore: finished");
+    }
+
+    private void backup(Uri uri) {
+        Log.d(TAG, "backup: started...");
+        BackupCoroutine backupCoroutine = new BackupCoroutine();
+        backupCoroutine.execute(uri);
+        Log.d(TAG, "backup: finished");
+    }
+
+    private void saveFile(Uri uri) {
+        Log.d(TAG, "saveFile: started...");
+        SaveFileCoroutine saveFileCoroutine = new SaveFileCoroutine();
+        saveFileCoroutine.execute(uri);
+        Log.d(TAG, "saveFile: finished");
+    }
+
+    private void getData() {
+        Log.d(TAG, "getData: started");
         data.clear();
         data.addAll(automaticBackup.getAllBackups());
         data.sort((o1, o2) -> Long.compare(o2.lastModified(), o1.lastModified()));
-        Log.d(TAG, "fillData: finished");
+        Log.d(TAG, "getData: finished");
     }
 
     private void selectFolderOld(int position) {
@@ -295,8 +289,8 @@ public class BackupAndRestore extends BaseActivity {
             try {
                 InputStream inputStream = getContentResolver().openInputStream(Uri.fromFile(selected));
                 assert inputStream != null;
-                String msg = Utils.readCSVfile(inputStream, getApplicationContext());
-                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+                boolean msg = Utils.readCsvFile(inputStream, getApplicationContext());
+                Toast.makeText(this, Boolean.toString(msg), Toast.LENGTH_SHORT).show();
             } catch (FileNotFoundException e) {
                 Log.e(TAG, "onActivityResult: file was not found on restore backup", e.fillInStackTrace());
                 e.printStackTrace();
@@ -330,9 +324,127 @@ public class BackupAndRestore extends BaseActivity {
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.putExtra(Intent.EXTRA_TITLE, from.getName());
-        intent.setType("text/*");
+        intent.setType(TYPE);
         backupToMove = from;
 
         startActivityForResult(intent, SAVE_AS_CODE);
+    }
+
+
+
+    private class RestoreCoroutine extends AsyncTaskCoroutine<Uri, Boolean> {
+        private static final String TAG = "RestoreCoroutine";
+
+        @Override
+        public void onPostExecute(@Nullable Boolean result) {
+            Log.d(TAG, POST_STARTED);
+            if (result) {
+                loadingDialog.setSuccessful();
+            } else {
+                loadingDialog.setError();
+            }
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(() -> {
+                loadingDialog.hideDialog();
+                setResult(MainActivity.RESULT_RESTORE_10_UP);
+            }, 1500);
+            Log.d(TAG, POST_FINISHED);
+        }
+
+        @Override
+        public void onPreExecute() {
+            Log.d(TAG, PRE_STARTED);
+            loadingDialog.showDialog();
+            Log.d(TAG, PRE_FINISHED);
+        }
+
+        @Override
+        public Boolean doInBackground(Uri... params) {
+            Log.d(TAG, DO_IN_BACKGROUND);
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(params[0]);
+                assert inputStream != null;
+                return Utils.readCsvFile(inputStream, getApplicationContext());
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, "onActivityResult: file was not found on restore backup", e.fillInStackTrace());
+                return false;
+            }
+        }
+    }
+
+    private class BackupCoroutine extends AsyncTaskCoroutine<Uri, Boolean> {
+        private static final String TAG = "BackupCoroutine";
+
+        @Override
+        public void onPostExecute(@Nullable Boolean result) {
+            Log.d(TAG, POST_STARTED);
+            if (result) {
+                loadingDialog.setSuccessful();
+            } else {
+                loadingDialog.setError();
+            }
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(() -> loadingDialog.hideDialog(), 1500);
+            Log.d(TAG, POST_FINISHED);
+        }
+
+        @Override
+        public void onPreExecute() {
+            Log.d(TAG, PRE_STARTED);
+            loadingDialog.showDialog();
+            Log.d(TAG, PRE_FINISHED);
+        }
+
+        @Override
+        public Boolean doInBackground(Uri... params) {
+            Log.d(TAG, DO_IN_BACKGROUND);
+            try {
+                OutputStream outputStream = getContentResolver().openOutputStream(params[0]);
+                assert outputStream != null;
+                return Utils.createCsvFile(outputStream, getApplicationContext());
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, "onActivityResult: file was not found on create backup", e.fillInStackTrace());
+                return false;
+            }
+        }
+    }
+
+    private class SaveFileCoroutine extends AsyncTaskCoroutine<Uri, Boolean> {
+        private static final String TAG = "SaveFileCoroutine";
+
+        @Override
+        public void onPostExecute(@Nullable Boolean result) {
+            Log.d(TAG, POST_STARTED);
+            if (result) {
+                loadingDialog.setSuccessful();
+            } else {
+                loadingDialog.setError();
+            }
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(() -> loadingDialog.hideDialog(), 1500);
+            Log.d(TAG, POST_FINISHED);
+        }
+
+        @Override
+        public void onPreExecute() {
+            Log.d(TAG, PRE_STARTED);
+            loadingDialog.showDialog();
+            Log.d(TAG, PRE_FINISHED);
+        }
+
+        @Override
+        public Boolean doInBackground(Uri... params) {
+            Log.d(TAG, DO_IN_BACKGROUND);
+            try {
+                OutputStream outputStream = getContentResolver().openOutputStream(params[0]);
+                assert outputStream != null;
+                outputStream.write(Files.readAllBytes(backupToMove.toPath()));
+                outputStream.close();
+                return true;
+            } catch (Exception e) {
+                Log.e(TAG, "onActivityResult: error when moving file from app storage to user defined location", e.fillInStackTrace());
+                return false;
+            }
+        }
     }
 }
